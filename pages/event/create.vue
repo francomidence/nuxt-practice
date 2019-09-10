566de37f5bd428854d2bc0680ed48220680561a4
francomidence <midencefranco@gmail.com>
2019-09-10 11:04:35 -0600

    First commit boyss

---
+++ pages/event/create.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'You can create new events for your neighborhood'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped></style>
